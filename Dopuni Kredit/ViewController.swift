13016012514bffa7e52ced178cd048c4deddab25
Vedad Sose <vedadsose@gmail.com>
2016-12-23 23:41:09 +0100

    Initial Commit

---
+++ Dopuni Kredit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dopuni Kredit
+//
+//  Created by Vedad Sose on 23/12/2016.
+//  Copyright © 2016 Vedad Sose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
